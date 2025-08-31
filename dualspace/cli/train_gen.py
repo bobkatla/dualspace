2aca3ee2b9237f8a397c4953c14d98e5473cfd7c
Duc Minh La <dminh.bob.la@gmail.com>
2025-08-31 11:24:09 +1000

    minors

--- dualspace/cli/train_gen.py
+++ dualspace/cli/train_gen.py
@@ -1,14 +1,10 @@
 from __future__ import annotations
 import click
-from pathlib import Path
-from dualspace.utils.seed import set_seed
-from dualspace.utils.config import load_config
-from dualspace.utils.env import dump_env_info
 from dualspace.train.train_generator import train
 
 @click.command()
 @click.option("--config", type=click.Path(exists=True, dir_okay=False, path_type=str), required=True)
 def train_gen(config: str):
     """Train conditional diffusion generator (CIFAR-10 first)."""
-    # TODO: load YAML, build data+models, train loop with EMA/ckpts
+    # TODO: build data+models, train loop with EMA/ckpts
     train(config)
